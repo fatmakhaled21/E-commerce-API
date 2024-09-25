87656a888370ec89ed828c07f31ab73eef8db807
fatmakhaled21 <fatma.khaled1820@gmail.com>
2024-09-25 12:25:16 +0300

    project added

---
+++ app/Models/Order.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $table = 'orders'; 
+    protected $fillable=[
+        'user_id',
+        'location_id',
+        'total_price',
+        'date_of_delivery',
+        'status'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
+  
+    public function location()
+    {
+        return $this->belongsTo(Location::class,'location_id');
+    }
+
+    public function items()
+    {
+        $this->hasMany(OrderItems::class);
+
+    }
+
+
+  
+
+}
